271b3efdd80a34061fa072aa4926e7efe0f37a5f
Tafadzwa <gandarit4467@gmail.com>
2022-10-17 16:27:31 -0700

    editing man 3 file

---
+++ 1-print_string.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_string - This function prints a string
+ * starting at a specific pointer.
+ * @str: this variable points to the string.
+ * Return: void
+ */
+int print_string(char *str)
+{
+	int i;
+	i = 0;
+	while (*str != '\0')
+	{
+		i += print_char(*str);
+		str += 1;
+	}
+	return (i);
+}
