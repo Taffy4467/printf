4b806d191fb72b4e2f00ec72e2c107d64f89083a
Tafadzwa <gandarit4467@gmail.com>
2022-10-17 16:19:02 -0700

    printf.c file

---
+++ printf.c
@@ -0,0 +1,48 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _printf - returns a string output to stdout with the common directives
+ * (%s, %c, %i, etc) accounted for.
+ * @format: refers to the string to output including all directives.
+ * Return: Returns the length (in bytes) of the string printed.
+ */
+int _printf(const char *format, ...)
+{
+	long int j, length, fmt_len;
+	va_list ap;
+
+	length = 0;
+	fmt_len = 0; /* will come from the replaced c, s, i, d. */
+	if (!format) /* if format is null */
+	{
+		return (-1);
+	}
+	else
+	{
+		va_start(ap, format);
+	}
+	for (j = 0; format[j] != '\0'; j++)
+	{
+		if (format[j] != '%') /* Any other char but % */
+		{
+			print_char(format[j]);
+			length++;
+		}
+		else
+		{
+			j++;
+			if (format[j] == '\0')
+			{
+				return (-1);
+			}
+			else
+			{
+				fmt_len += replace(format[j], ap);
+			}
+		}
+	}
+	va_end(ap);
+	return (length + fmt_len);
+}
